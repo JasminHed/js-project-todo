45abb44ea07ef15de164747c3a58ead0487add0a
Jasmin Hedlund <jasminhedlund@gmail.com>
2025-05-27 15:53:59 +0200

    changed function app to arrow function for consistency

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import ToDo from "./components/ToDo.jsx";
 import GlobalStyle from "./styling/GlobalStyle.jsx";
 
-function App() {
+const App = () => {
   return (
     <>
       <GlobalStyle />
       <ToDo />
     </>
   );
-}
+};
 
 export default App;
